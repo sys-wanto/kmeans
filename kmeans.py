bee5d8d348a64e078bac20e565ac202b4420c760
sys-wanto <nckdostrow@gmail.com>
2023-07-27 23:00:11 +0700

    first init

---
+++ kmeans.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+from IPython.core.display import Math
+import math
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/tfidf', methods=['POST'])
+def index():
+
+    # 1
+    q = request.form.getlist('q[]')
+
+    d_string = (' ').join(q)
+    # 2
+    string_splited = d_string.split()
+
+    # 3
+    kamus_kata = {}
+    for string in string_splited:
+        kamus_kata[string] = d_string.count(string)
+
+    # 4
+    data_dict = {}
+    data_list = []
+    for kalimat in q:
+        for kata in string_splited:
+            data_dict[kata] = kalimat.count(kata)
+            data_list.append(data_dict)
+            data_dict = {}
+
+    # 5
+    term_frequencies = []
+    for i in range(len(data_list)):
+        term_frequencies.append(f"Data {i+1} : {data_list[i]}")
+
+    # 6
+    data_idf = {}
+    for kata in kamus_kata:
+        hitung = math.log(len(q)/kamus_kata[kata], 10)
+        data_idf[kata] = hitung
+    # 7
+    hasil = []
+    data_sementara = {}
+    for index in range(len(data_list)):
+        for i in data_list[index]:
+            hitung = data_list[index][i]*data_idf[i]
+            data_sementara[i] = round(hitung, 3)
+            hasil.append(data_sementara)
+
+    return jsonify(
+        is_error=False,
+        data=hasil,
+        msg="sukses."
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
